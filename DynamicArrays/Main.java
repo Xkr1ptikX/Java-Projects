5a1770a66481930477ad4e4dc0ef603b4ae5e469
Xkr1ptikX <thorin904@yahoo.com>
2015-10-24 11:26:16 -0400

    initial push for DynamicArrays

---
+++ DynamicArrays/Main.java
@@ -0,0 +1,17 @@
+package demo1;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<String> fruits = new ArrayList();
+		fruits.add("apple");
+		fruits.add("pear");
+		fruits.add("orange");
+		fruits.add("banana");
+		
+		System.out.println(fruits.size());
+	}
+
+}
