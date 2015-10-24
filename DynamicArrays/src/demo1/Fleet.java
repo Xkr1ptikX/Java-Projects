ea8bc3e41e8e216ccee5019180f06207dbef35b2
Xkr1ptikX <thorin904@yahoo.com>
2015-10-24 13:16:10 -0400

    added new class Fleet and adjusted Main accordingly

---
+++ DynamicArrays/src/demo1/Fleet.java
@@ -0,0 +1,18 @@
+/**
+ * not sure if ArrayList already has some sort of print method
+ */
+
+package demo1;
+
+import java.util.ArrayList;
+
+public class Fleet extends ArrayList{
+	
+	public void printFleet() {
+		for (int i = 0; i < this.size(); i++) {
+			System.out.println("Car " + (i + 1) + ": ");
+			((Car) this.get(i)).printCar();
+			System.out.println();
+		}
+	}
+}
--- DynamicArrays/src/demo1/Main.java
+++ DynamicArrays/src/demo1/Main.java
@@ -1,18 +1,24 @@
 package demo1;
 
-import java.util.ArrayList;
-
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Car> fleet = new ArrayList<Car>();
+		Fleet fleet = new Fleet();
+		
 		Car car1 = new Car("Toyota", "Tacoma", "2008", "white");
+		Car car2 = new Car("Toyota", "Rav4", "2010", "black");
+		Car car3 = new Car("Honda", "Civic", "2015", "red");
+		Car car4 = new Car("Toyota", "Corolla", "2015", "white");
+		Car car5 = new Car("Toyota", "Camry", "2016", "blue");
 		
+		/** not sure what these warnings are about **/
 		fleet.add(car1);
+		fleet.add(car2);
+		fleet.add(car3);
+		fleet.add(car4);
+		fleet.add(car5);
 		
-		System.out.println("Current size of fleet: " + fleet.size());
-		
-		fleet.get(0).printCar();
+		fleet.printFleet();
 	}
 
 }
